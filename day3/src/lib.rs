0facc77d7643fa186ca156ff51e7a9ee492c3696
Dewaeq <54279351+Dewaeq@users.noreply.github.com>
2022-12-03 13:03:31 +0100

    day 3

---
+++ day3/src/lib.rs
@@ -0,0 +1,64 @@
+use std::collections::HashSet;
+
+pub fn part_one(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|sack| {
+            let chars = sack.chars().collect::<Vec<_>>();
+            let (first, second) = chars.split_at(sack.len() / 2);
+            first
+                .iter()
+                .filter(|&item| second.contains(item))
+                .collect::<HashSet<_>>()
+                .iter()
+                .map(|&priority| {
+                    let x = *priority as u8;
+                    if x < 91 {
+                        (x - 38) as u32
+                    } else {
+                        (x - 96) as u32
+                    }
+                })
+                .sum::<u32>()
+        })
+        .sum()
+}
+
+pub fn part_two(input: &str) -> u32 {
+    input
+        .lines()
+        .collect::<Vec<_>>()
+        .chunks_exact(3)
+        .map(|group| {
+            let badge = group[0]
+                .chars()
+                .find(|&item| group[1].contains(item) && group[2].contains(item))
+                .unwrap();
+            let x = badge as u8;
+            if x < 91 {
+                (x - 38) as u32
+            } else {
+                (x - 96) as u32
+            }
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_test() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
+        let result = 157;
+        assert_eq!(result, part_one(input));
+    }
+
+    #[test]
+    fn part_two_test() {
+        let input = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
+        let result = 70;
+        assert_eq!(result, part_two(input));
+    }
+}
